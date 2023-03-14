db8db8ecbc169188b675b8990e52f060d1ef50f5
Chiara-Vanderlan <115263438+Chiara-Vanderlan@users.noreply.github.com>
2023-03-14 19:42:47 +0530

    add

---
+++ OnlineMovieTicketBookingProject/OnlineMovieTicketBookingProject/Models/BookingTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineMovieTicketBookingProject.Models
+{
+    public class BookingTable
+    {
+        public int Id { get; set; }
+        public string seatno { get; set; }
+        public string UserId { get; set; }
+        public DateTime DateTopresent { get; set; }
+        public int MovieDetailsId { get; set; }
+        public int Amount { get; set; }
+
+        [ForeignKey("MovieDetailsId")]
+        public virtual MovieDetails moviedetails { get; set; }
+    }
+}
---
+++ OnlineMovieTicketBookingProject/OnlineMovieTicketBookingProject/Models/MovieDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineMovieTicketBookingProject.Models
+{
+    public class MovieDetails
+    {
+        public int Id { get; set; }
+        public string Movie_Name { get; set; }
+        public string Movie_description { get; set; }
+        public DateTime DateAndTime{ get; set; }
+        public string MoviePicture { get; set; }
+
+        public virtual ICollection<BookingTable> booking { get; set; }
+    }
+}
