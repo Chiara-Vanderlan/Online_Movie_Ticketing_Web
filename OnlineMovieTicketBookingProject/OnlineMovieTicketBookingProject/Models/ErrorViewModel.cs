970b44d0ca1b08441a8b88ca337f9f1fb6ca5103
Chiara-Vanderlan <115263438+Chiara-Vanderlan@users.noreply.github.com>
2023-03-15 20:17:39 +0530

    add front end

--- OnlineMovieTicketBookingProject/OnlineMovieTicketBookingProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineMovieTicketBookingProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
