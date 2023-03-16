1ffe9875a4dded556b77aa4a5dacd44dd1e7ed77
Chiara-Vanderlan <115263438+Chiara-Vanderlan@users.noreply.github.com>
2023-03-16 20:53:59 +0530

    add changes

---
+++ frontend/src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './content.css'
+
+export default function () {
+  return (
+    <div className ="Paragraph"> 
+        <p>Paragraphs are medium-sized units of writing, longer than sentences, but shorter than sections, chapters, or entire works. Because they connect the “small” ideas of individual sentences to a “bigger” idea, paragraph structure is essential to any writing for organization, flow, and comprehension. </p>
+    </div>
+  )
+}
